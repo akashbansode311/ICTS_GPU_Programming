7b13fb0687ad4e442393c77564dd088502edd505
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2025-10-06 11:18:18 +0530

    Update 01.Hello_World.cu

--- 01.Hello_World.cu
+++ 01.Hello_World.cu
@@ -1,2 +1,21 @@
 #include <stdio.h>
-#include <>
+
+// Kernel function to print "Hello, world!" from the GPU
+__global__ void helloFromGPU()
+{
+    printf("Hello, world from GPU\n");
+}
+
+int main()
+{
+    // Print "Hello, World!" from the CPU
+    printf("Hello, World from CPU\n");
+
+    // Launch kernel with a single thread to print "Hello, World!" from the GPU
+    helloFromGPU<<<1, 10>>>();
+
+    // Synchronize to ensure all printf statements from the GPU are executed
+    cudaDeviceSynchronize();
+
+    return 0;
+}
